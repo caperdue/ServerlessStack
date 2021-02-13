d1ba689925ee86df6f0e6b9d204fd28e0348596e
Carly Perdue <carlyperdue@gmail.com>
2021-02-13 09:32:08 -0500

    Adding CORS to our Serverless API

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,21 +1,24 @@
 //Handler function to handle all HTTP responses in one place
 export default function handler(lambda) {
-    return async function (event, context) {
-        let body, statusCode;
+  return async function (event, context) {
+    let body, statusCode;
 
-        try {
-            //Run async lambda function
-            body = await lambda(event, context);
-            statusCode = 200;
-        }
-        catch (e) {
-            body = {error: e.message};
-            statusCode = 500;
-            console.log(e);
-        }
-        return {
-            statusCode,
-            body: JSON.stringify(body),
-        };
+    try {
+      //Run async lambda function
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      body = { error: e.message };
+      statusCode = 500;
+      console.log(e);
+    }
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     };
+  };
 }
